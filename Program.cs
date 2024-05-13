a559578c8e67afe82bf85f66323f02222be111a2
GitHubUser10F2C <nelson.soto@alumnos.ucn.cl>
2024-05-13 16:00:36 -0400

    feat: add endpoints

--- Program.cs
+++ Program.cs
@@ -10,12 +10,128 @@ var app = builder.Build();
 var ebooks = app.MapGroup("api/ebook");
 
 // TODO: Add more routes
-ebooks.MapPost("/", CreateEBookAsync);
+ebooks.MapPost("/", CreateEBook);
+ebooks.MapGet("/", GetEbooks);
+ebooks.MapPut("/{id}", UpdateEbook);
+ebooks.MapPut("/{id}/change-availability", ChangeAvailability);
+ebooks.MapPut("/{id}/increment-stock", IncrementStock);
+ebooks.MapPost("/purchase", Purchase);
+ebooks.MapDelete("/{id}", DeleteEbook);
 
 app.Run();
 
 // TODO: Add more methods
-async Task<IResult> CreateEBookAsync(DataContext context)
+static IResult CreateEBook(DataContext context, EBook ebook)
 {
-    return Results.Ok();
+    if (context.EBooks.Any(x => x.Title == ebook.Title))
+    {
+        if (context.EBooks.Any(x => x.Author == ebook.Author))
+        {
+            return TypedResults.BadRequest("Un ebook con el mismo título y autor ya existe");
+        }
+    }
+
+    ebook.IsAvailable = true;
+    ebook.Stock = 0;
+
+    context.EBooks.Add(ebook);
+    context.SaveChanges();
+
+    return TypedResults.Created($"/ebook/{ebook.Id}", ebook);
+}
+
+static IResult GetEbooks(DataContext context, [FromQuery]string genre, [FromQuery]string author, [FromQuery]string format)
+{
+
+    return TypedResults.Ok(context.EBooks.ToArray());
+}
+
+static IResult UpdateEbook(int id, DataContext context, EBook inputEbook)
+{
+    var ebook = context.EBooks.Find(id);
+
+    if (ebook is null)
+        return TypedResults.NotFound();
+
+    if (context.EBooks.Any(x => x.Title == inputEbook.Title))
+    {
+        if (context.EBooks.Any(x => x.Author == inputEbook.Author))
+        {
+            return TypedResults.BadRequest("Un ebook con el mismo título y autor ya existe, no se pudo actualizar");
+        }
+    }
+
+    ebook.Title = inputEbook.Title;
+    ebook.Author = inputEbook.Author;
+    ebook.Genre = inputEbook.Genre;
+    ebook.Format = inputEbook.Format;
+    ebook.Price = inputEbook.Price;
+
+    context.SaveChangesAsync();
+
+    return TypedResults.NoContent();
+}
+
+static IResult ChangeAvailability(int id, DataContext context)
+{
+    var ebook = context.EBooks.Find(id);
+
+    if (ebook is null)
+        return TypedResults.NotFound();
+
+    ebook.IsAvailable = !ebook.IsAvailable;
+
+    context.SaveChangesAsync();
+
+    return TypedResults.NoContent();
+}
+
+static IResult IncrementStock(int id, [FromBody]int stock, DataContext context)
+{
+    var ebook = context.EBooks.Find(id);
+
+    if (ebook is null)
+        return TypedResults.NotFound();
+
+    ebook.Stock += stock;
+
+    context.SaveChangesAsync();
+
+    return TypedResults.NoContent();
+}
+
+static IResult Purchase(int id, int stockToBuy, int amountToPay, DataContext context)
+{
+    var ebook = context.EBooks.Find(id);
+
+    if (ebook is null)
+        return TypedResults.NotFound();
+
+    if (ebook.Stock < stockToBuy)
+    {
+        return TypedResults.BadRequest("No hay suficiente stock para cubrir la compra");
+    }
+
+    if (ebook.Price * stockToBuy != amountToPay)
+    {
+        return TypedResults.BadRequest("El monto a pagar no corresponde con el calculado por el sistema");
+    }
+
+    ebook.Stock -= stockToBuy;
+
+    context.SaveChangesAsync();
+
+    return TypedResults.NoContent();
+}
+
+static IResult DeleteEbook(int id, DataContext context)
+{
+    if (context.EBooks.Find(id) is EBook eBook)
+    {
+        context.EBooks.Remove(eBook);
+        context.SaveChangesAsync();
+        return TypedResults.NoContent();
+    }
+
+    return TypedResults.NotFound();
 }
